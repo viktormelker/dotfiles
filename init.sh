df31c82df92e9a15b78ffa9c3c1562eff4b3bd42
Viktor Melker <viktormelker@gmail.com>
2017-10-21 11:55:09 +0200

    Added comment about problem i had and a workaround

--- init.sh
+++ init.sh
@@ -5,6 +5,9 @@ git config --global user.email "viktormelker@gmail.com"
 git config --global user.name "Viktor Melker"
 git config --global gpg.program gpg2
 
+# solved a problem i had with commiting. See https://github.com/desktop/desktop/issues/1391
+#git config commit.gpgsign false
+
 # create dirs
 mkdir -p $HOME/repos
 mkdir -p $HOME/bin
