93ac9b2a9c076b3c4c7119a6cf966582ff1f5c35
Viktor Melker <viktormelker@gmail.com>
2017-10-21 12:54:50 +0200

    removed faulty function

--- .config/fish/functions/venv.fish
+++
@@ -1,8 +0,0 @@
-function venv --description 'create or activate a virtualenv'
-    if not test -z "$argv"
-        virtualenv --prompt "($argv) " .venv
-        source .venv/bin/activate.fish
-    else
-        source .venv/bin/activate.fish
-    end
-end
