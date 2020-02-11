5f7735ef7045b41cc2337d09c27c3f29e54763aa
Viktor Melker <viktormelker@gmail.com>
2020-02-11 22:26:44 +0100

    Add autolock of screen

--- init_arch.sh
+++ init_arch.sh
@@ -109,6 +109,6 @@ sudo pacman -S mariadb
 yay -S direnv
 sudo pacman -S okular
 sudo pacman -S yarn
-sudo pacman -S i3-vm i3status-manjaro dmenu-manjaro i3-wallpapers i3lock pcmanfm network-manager-applet feh rofi py3status
+sudo pacman -S i3-vm i3status-manjaro dmenu-manjaro i3-wallpapers i3lock pcmanfm network-manager-applet feh rofi py3status xautolock
 sudo pacman -S tldr
 sudo pacman -S kio-gdrive
