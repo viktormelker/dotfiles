7e51915a8f4aa2b5f571fd77e24c7746152e1a84
Viktor Melker <viktormelker@gmail.com>
2020-08-17 09:58:51 +0200

    Add display management scripts

---
+++ scripts/display/home_dual.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output HDMI-A-0 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --scale 1x1 --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate normal --scale 2x2
---
+++ scripts/display/single_screen.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+xrandr --output HDMI-A-0 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --scale 1x1 --output DVI-D-0 --off
