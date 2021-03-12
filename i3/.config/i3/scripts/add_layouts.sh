3113c73ff59efb8a18eaabeb71b1d8b8b3453bea
Viktor Melker <viktormelker@gmail.com>
2021-03-12 22:24:32 +0100

    prepare for autolaunch i3 programs

---
+++ i3/.config/i3/scripts/add_layouts.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+i3-msg "workspace Social; append_layout ~/.config/i3/workspace/workspace_1.json"
+i3-msg "workspace Social; append_layout ~/.config/i3/workspace/workspace_2.json"
+i3-msg "workspace Social; append_layout ~/.config/i3/workspace/workspace_8.json"
+i3-msg "workspace Social; append_layout ~/.config/i3/workspace/workspace_9.json"
