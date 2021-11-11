1a8acb692c9626110effe384091395e10a70e56f
Viktor Melker <viktormelker@gmail.com>
2021-11-11 17:06:17 +0100

    Add firmware update tool

--- init_arch.sh
+++ init_arch.sh
@@ -92,6 +92,7 @@ sudo pacman -S unzip
 sudo pacman -S autorandr
 sudo pacman -S yq
 sudo pacman -S gnome-keyring seahorse
+yay -S fwupd
 
 # bluetooth
 sudo pacman -S bluez-utils pulseaudio-bluetooth blueman
@@ -157,6 +158,7 @@ sudo pacman -S swig
 yay -S neo4j-community
 sudo pacman -S redis
 sudo pacman -S postgresql
+
 sudo pacman -S mariadb
 
 sudo pacman -S terraform
