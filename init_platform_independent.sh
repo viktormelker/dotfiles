d315af15e5a33b20e9db171e0484ee3466ccc47d
Viktor Melker <viktormelker@gmail.com>
2022-03-13 13:34:18 +0100

    remove platform independent installs

--- init_platform_independent.sh
+++ init_platform_independent.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 # install all cross plattform stuff here
 
-git config --global user.email "viktormelker@gmail.com"
-git config --global user.name "Viktor Melker"
-git config --global gpg.program gpg2
-
 pip install --upgrade pip
 
 mkdir ~/dev
@@ -22,15 +18,6 @@ curl https://pyenv.run | bash
 # fzf, needed for some of the tmux stuff!
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-
-# Nix package manager
-curl https://nixos.org/nix/install | sh
-. /home/viktor/.nix-profile/etc/profile.d/nix.sh
-nix-env -i diff-so-fancy
-nix-env -i ripgrep
-
-# zsh
-mkdir ~/.zsh
 
 # vim
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
