8c06e0682bc3ba8037d3b124bf7ca67c9a6d507b
Viktor Melker <viktormelker@gmail.com>
2022-03-14 21:22:16 +0100

    Autostart spotify

--- i3/.config/i3/scripts/add_layouts.sh
+++ i3/.config/i3/scripts/add_layouts.sh
@@ -2,3 +2,4 @@
 i3-msg "workspace 1: Web; append_layout ~/.config/i3/workspace/workspace_1.json"
 i3-msg "workspace 2: Terminal; append_layout ~/.config/i3/workspace/workspace_2.json"
 i3-msg "workspace 9: Social; append_layout ~/.config/i3/workspace/workspace_9.json"
+i3-msg "workspace 10: Media; append_layout ~/.config/i3/workspace/workspace_10.json"
