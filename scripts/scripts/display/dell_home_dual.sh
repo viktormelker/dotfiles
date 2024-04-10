ddc5c4f98bf9b326fe47391be0cf8cc728ea7bf2
Viktor Melker <viktormelker@gmail.com>
2024-04-10 08:03:18 +0200

    Fix screen settings with xrandr

--- scripts/scripts/display/dell_home_dual.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-xrandr --output DP3-1 --mode 2560x1440 --scale 1.5x1.5 --output DP3-2 --mode 2560x1440 --scale 1.5x1.5
--- scripts/scripts/display/home_dual.sh
+++ scripts/scripts/display/home_dual.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr --output HDMI-A-0 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --scale 1x1 --output DVI-D-0 --mode 1920x1080 --pos 0x0 --rotate normal --scale 2x2
+xrandr --output DP-3-1 --mode 3840x2160 --scale 1x1 --output DP-3-2 --mode 3840x2160 --pos 3840x0 --scale 1x1
