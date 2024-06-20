e2f8b69621f3d5af8456b3816e0863fea66084cd
Viktor Melker <viktormelker@gmail.com>
2024-06-20 10:21:50 +0200

    updates

--- scripts/scripts/display/single_screen.sh
+++ scripts/scripts/display/single_screen.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-xrandr --output DP-3-1 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --scale 1x1 --output DP-3-2 --off
+xrandr --output DP-3-1 --primary --mode 3840x2160 --pos 3840x0 --rotate normal --scale 1x1 --dpi 164 --output DP-3-2 --off --output eDP-1 --off
