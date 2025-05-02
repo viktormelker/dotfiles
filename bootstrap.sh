c3853c976f851af5cc3c6b61f9bd23990a049d12
Viktor Melker <viktormelker@gmail.com>
2025-05-02 07:21:20 +0200

    Add pgcli config

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-stow --target ~/ base fzf git i3 python scripts tmux vim vscode zsh starship
+stow --target ~/ base fzf git i3 python scripts tmux vim vscode zsh starship pgcli
