8ec41d7afd1e5ed9fd5c07a7f06a501b527fba87
Viktor Melker <viktormelker@gmail.com>
2025-12-16 22:38:07 +0100

    readd i3 config

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin master;
 
-stow --target ~/ base fzf git python scripts tmux nvim vscode zsh pgcli autorandr warp
+stow --target ~/ base fzf git python scripts tmux nvim vscode zsh pgcli autorandr warp i3
