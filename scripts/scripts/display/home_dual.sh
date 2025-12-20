0c4b50bc9b83283fa43185fb3371b0375eb3f9b7
Viktor Melker <viktormelker@gmail.com>
2025-12-20 19:08:57 +0100

    Remove custom screen display scripts

--- scripts/scripts/display/home_dual.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-xrandr --output DP-3-1 --mode 2560x1440 --scale 1x1 --output DP-3-2 --mode 2560x1440 --pos 2560x0 --scale 1x1 --output eDP-1 --off --dpi 120
--- scripts/scripts/display/single_screen.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-xrandr --output DP-3-1 --primary --mode 2560x1440 --pos 0x0 --rotate normal --scale 1x1 --dpi 168 --output DP-3-2 --off --output eDP-1 --off
