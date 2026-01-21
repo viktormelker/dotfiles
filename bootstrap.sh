6f77b269f73d4e6177563ed6ed981fe8b1f51e50
Viktor Melker <viktormelker@gmail.com>
2026-01-21 08:31:37 +0100

    Setup cursor settings

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,3 +5,7 @@ cd "$(dirname "${BASH_SOURCE}")";
 git pull origin master;
 
 stow --target ~/ base fzf git python scripts tmux nvim vscode zsh pgcli autorandr warp
+
+# Add Cursor settings, same as VSCode
+ln -sf ~/dev/dotfiles/vscode/.config/Code/User/settings.json ~/.config/Cursor/User/settings.json
+ln -sf ~/dev/dotfiles/vscode/.config/Code/User/keybindings.json ~/.config/Cursor/User/keybindings.json
